a2956f4a78436952a0743e90b58ef9b9c5f8e545
Alexander Green <alexander.green@logius.nl>
2026-07-20 14:02:02 +0200

    Release 1.0.0

--- js/config.mjs
+++ js/config.mjs
@@ -2,15 +2,12 @@ import { loadRespecWithConfiguration } from "https://logius-standaarden.github.i
 import { generateMermaidFigures } from "https://logius-standaarden.github.io/publicatie/respec/plugins/mermaid.mjs";
 
 loadRespecWithConfiguration({
-  useLogo: true,
-  useLabel: true,
-  license: "cc-by",
-  specStatus: "WV",
+  specStatus: "DEF",
   specType: "ST",
   pubDomain: "ftv",
   shortName: "adl",
-  publishDate: "2024-05-19",
-  publishVersion: "0.0.1",
+  publishDate: "2026-06-25",
+  publishVersion: "1.0.0",
   spellcheck: false,
   // TODO: verwijder voor publicatie
   prevVersion: [],
